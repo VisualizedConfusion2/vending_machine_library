f9c51374980e60a216ac104a09e2e3e09db420f3
VisualizedConfusion2 <thh005@edu.zealand.dk>
2025-08-28 19:11:04 +0200

    fixede en fejl

--- vending_machine_library/Repos/TransactionRepo.cs
+++ vending_machine_library/Repos/TransactionRepo.cs
@@ -3,11 +3,4 @@ using vending_machine_library.Models;
 
 namespace vending_machine_library.Interfaces
 {
-    public interface ITransactionRepo
-    {
-        List<Transaction> GetAll();                // Get all transactions
-        void Add(Transaction transaction);         // Record a new transaction
-        Transaction GetByID(int id);               // Find a specific transaction
-        List<Transaction> GetByProduct(int productId); // Get all transactions for a product
-    }
 }
