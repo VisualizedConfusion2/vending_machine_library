909153a2e97a78778ed08dd6ace89265b8d703d8
mal025 <mal025@edu.zealand.dk>
2025-08-28 19:33:13 +0200

    Added the slot class

---
+++ vending_machine_library/Models/Slot.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vending_machine_library.Models
+{
+    public class Slot
+    {
+        public List<Product> Products = new List<Product>(); // List of products
+        public int Price; // Price for the product
+
+        public Product EjectProduct() // Gives the product to the user
+        {
+            Product product = Products[0];
+            Products.RemoveAt(0);
+            return product;
+        }
+
+        public void FillProduct(Product product) // Fills a new product in the slots
+        {
+            if(Products.Count < 8) // Product cap is 8
+            {
+                Products.Add(product);
+            }
+            // Todo: Add custom exception when product cap has reached
+        }
+    }
+}
