fab25855a64d5098ec5037fed0e2395f4a04a071
mal025 <mal025@edu.zealand.dk>
2025-08-29 10:04:17 +0200

    Added chips

---
+++ vending_machine_library/Models/Chips.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vending_machine_library.Models
+{
+    public class Chips: Product
+    {
+
+        public Chips(): base("Chips") // Calls the base constructor with "Chips" as the name
+        {
+
+        }
+    }
+}
