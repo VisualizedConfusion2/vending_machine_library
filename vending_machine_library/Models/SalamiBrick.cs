794ae7aaa6bdf4d0f9a764123669c2739e880eeb
T04 <thh005@edu.zealand.dk>
2025-08-29 10:31:23 +0200

    salami brick

---
+++ vending_machine_library/Models/SalamiBrick.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vending_machine_library.Models
+{
+    public class SalamiBrick: Product
+    {
+
+        public SalamiBrick(): base("Salami Brick") // Calls the base constructor with "Salami Brick" as the name
+        {
+
+        }
+    }
+}
