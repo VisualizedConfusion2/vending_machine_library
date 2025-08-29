cb02748c843150ed27fafb8d01cc4920663f8200
T04 <thh005@edu.zealand.dk>
2025-08-29 10:49:17 +0200

    Produkter tilføjet

---
+++ vending_machine_library/Models/Lasagna.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vending_machine_library.Models
+{
+    public class Lasagna: Product
+    {
+
+        public Lasagna(): base("Lasagna") // Calls the base constructor with "Lasagna" as the name
+        {
+
+        }
+    }
+}
---
+++ vending_machine_library/Models/WhiteMonster.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vending_machine_library.Models
+{
+    public class WhiteMonster: Product
+    {
+
+        public WhiteMonster(): base("WhiteMonster") // Calls the base constructor with "WhiteMonster" as the name
+        {
+
+        }
+    }
+}
