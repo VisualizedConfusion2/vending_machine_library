c24340d396088ed1c0e4f6d74dc91797e47b90a2
mal025 <mal025@edu.zealand.dk>
2025-09-18 11:22:59 +0200

    Added unittest

---
+++ VendingMachineTests1/Models/VendingmachineTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using vending_machine_library.Models;
+
+namespace VendingmachineTests
+{
+    [TestClass()]
+    public class VendingmachineTests
+    {
+
+        private Vendingmachine _vendingMachine;
+        [TestInitialize]
+        public void Setup()
+        {
+            _vendingMachine = new Vendingmachine();
+        }
+
+        [TestMethod]
+        public void Buy_Slot0WithEnoughCoins_ReturnsChips()
+        {
+            // Arrange
+            double[] coins = { 10, 10, 5 }; // total 25
+
+            // Act
+            Product result = _vendingMachine.Buy(0, coins);
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Chips", result.Name);
+        }
+
+
+    }
+}
